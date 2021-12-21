89bfa63f2a375fe5298ea7eef98e95f19e9e314a
fl4pj4ck <379854+fl4pj4ck@users.noreply.github.com>
2021-12-21 18:08:43 +0000

    get/set for config()

--- wallpapers.py
+++ wallpapers.py
@@ -4,14 +4,21 @@ from send2trash import send2trash
 config_path = os.path.dirname(os.path.realpath(sys.argv[0]))
 config_file = os.path.join(config_path, 'wallpapers.ini')
 
+def get_config(config, setting):
+    # read source folder from wallpapers.cfg
+    config.read(config_file)
+    return(config.get('DEFAULT', setting))    
+
+def set_config(config, name, setting):
+    config['DEFAULT'][name] = setting
+    with open(config_file, 'w') as configfile:    
+        config.write(configfile)
+
 # next_wallpaper(): 
 # pick a new wallpaper from _folder_ and set it as current background
 # + update _ini_ file with its path
-def next_wallpaper():
-    # read source folder from wallpapers.cfg
-    config = configparser.ConfigParser()
-    config.read(config_file)
-    wallpaper_location = config.get('DEFAULT', 'folder')
+def next_wallpaper(config):
+    wallpaper_location = get_config(config, 'folder')
     # get file list, only .jpg and .jpeg
     files = os.listdir(wallpaper_location)
     backgrounds = [file for file in files if file.endswith(( '.jpg', '.jpeg', '.png'))]
@@ -21,57 +28,43 @@ def next_wallpaper():
         if os.path.isfile(new_wallpaper):
             ctypes.windll.user32.SystemParametersInfoW(20, 0, new_wallpaper , 0)
             # update config file with the location of current wallpaper
-            config['DEFAULT']['last'] = new_wallpaper
-            with open(config_file, 'w') as configfile:    
-                config.write(configfile)
+            set_config(config, 'last', new_wallpaper)
 
 # delete_wallpaper():
 # get current background path from _ini_ and delete
 # + manually call next_wallpaper()
-def delete_wallpaper():
-    # read source folder from wallpapers.cfg
-    config = configparser.ConfigParser()
-    config.read(config_file)
-    wallpaper_location = config.get('DEFAULT', 'last')
+def delete_wallpaper(config):
+    wallpaper_location = get_config('last')
     if os.path.isfile(wallpaper_location):
         send2trash(wallpaper_location)
-    next_wallpaper()
+    next_wallpaper(config)
 
 # set_path():
 # update wallpapers folder in _ini_
 # + manually call next_wallpaper() 
-def set_path(new_path):
+def set_path(config, new_path):
     if os.path.isdir(new_path):
-        config = configparser.ConfigParser()
-        config.read(config_file)
-        config['DEFAULT']['folder'] = new_path
-        with open(config_file, 'w') as configfile:    
-            config.write(configfile)
-        next_wallpaper()
+        set_config(config, 'folder', new_path)
+        next_wallpaper(config)
 
 # check_config():
 # look up _ini_ file and if it doesn't exist - create
 # + if no folder reference - create using getcwd()
-def check_config():
+def check_config(config):
     if not os.path.isfile(config_file):
-        config = configparser.ConfigParser()
-        config['DEFAULT']['folder'] = config_path
-        config['DEFAULT']['last'] = ''
-        with open(config_file, 'w') as configfile:    
-            config.write(configfile)
+        set_config(config, 'folder', config_path)
+        set_config(config, 'last', '')
     else:
-        config = configparser.ConfigParser()
-        config.read(config_file)
-        wallpaper_folder = config['DEFAULT']['folder']
-        if not os.path.isdir(wallpaper_folder):
-            config['DEFAULT']['folder'] = config_path
-            with open(config_file, 'w') as configfile:    
-                config.write(configfile)
+        if not os.path.isdir(get_config(config, 'folder')):
+            set_config(config, 'folder', config_path)
+        if not os.path.isdir(get_config(config, 'last')):
+            set_config(config, 'last', '')            
 
 # run_main()
 # where the magic happens
 def run_main():
-    check_config()
+    config = configparser.ConfigParser()
+    check_config(config)
     # Initiate the parser
     parser = argparse.ArgumentParser()
     parser.add_argument("-D", "--delete", action="store_true")
@@ -79,11 +72,11 @@ def run_main():
     # Read arguments from the command line
     args = parser.parse_args()
     if args.delete:
-        delete_wallpaper()
+        delete_wallpaper(config)
     elif args.path:
-        set_path(args.path)
+        set_path(config, args.path)
     else:
-        next_wallpaper()
+        next_wallpaper(config)
 
 if __name__ == "__main__":
     run_main()
